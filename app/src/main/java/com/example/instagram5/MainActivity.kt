759b6b5a47f21ec2b393042a39c74dc01a6b1809
Akshay@2003 <akshay88magar@gmail.com>
2023-04-20 00:36:06 +0530

    Profile Fragment Working Done

--- app/src/main/java/com/example/instagram5/MainActivity.kt
+++ app/src/main/java/com/example/instagram5/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.heart
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,R.id.heart,R.id.profile
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
--- app/src/main/java/com/example/instagram5/ui/Profile/ProfileFragment.kt
+++ app/src/main/java/com/example/instagram5/ui/Profile/ProfileFragment.kt
@@ -6,27 +6,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.instagram5.R
+import com.example.instagram5.databinding.FragmentDashboardBinding
+import com.example.instagram5.databinding.FragmentProfileBinding
+import com.example.instagram5.ui.dashboard.DashboardViewModel
 
 class ProfileFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
+    private var _binding: FragmentProfileBinding? = null
 
-    private lateinit var viewModel: ProfileViewModel
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+        _binding = FragmentProfileBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textProfile
+        profileViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
+
 
 }
