500492d0d0435b5407df59a0e4c4cf2d05548a92
Akshay@2003 <akshay88magar@gmail.com>
2023-04-22 02:18:26 +0530

    reels Adapter Updated .

--- app/src/main/java/com/example/instagram5/ui/heart/VedioAdapter.kt
+++ app/src/main/java/com/example/instagram5/ui/heart/VedioAdapter.kt
@@ -1,17 +1,41 @@
 package com.example.instagram5.ui.heart
 
+import android.content.Context
+import android.provider.MediaStore
 import android.view.View
-import android.widget.ImageView
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram5.R
-import com.example.instagram5.ui.dashboard.ItemAdapter
 
 
-    class VedioAdapter(private val itemList:List<Vedio>): RecyclerView.Adapter<com.example.instagram5.ui.heart.VedioAdapter.VedioHolder>(){
+class VideoAdapter(
+    var context: Context,
+    var videos: ArrayList<MediaStore.Video>,
+    var videoPreparedListener: OnVideoPreparedListener
+) : RecyclerView.Adapter<VideoAdapter.VideoViewHolder>() {
+    class VideoViewHolder ( val binding: ListVideoBinding,
+                            var context: Context,
+                            var videoPreparedListener: OnVideoPreparedListener){
 
-        class VedioViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
-            var vedioView:  =itemView.findViewById(R.id.ved)
-        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): VideoAdapter.VideoViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: VideoAdapter.VideoViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+}
 
 
 
