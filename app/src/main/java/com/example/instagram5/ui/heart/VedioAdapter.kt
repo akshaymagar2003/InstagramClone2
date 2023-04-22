55ca3a04e771c49458c3b54364adab40fdbdc00d
Akshay@2003 <akshay88magar@gmail.com>
2023-04-22 11:56:31 +0530

    Exoplayer Builder is created and OnPlayerError Event is handled

--- app/src/main/java/com/example/instagram5/ui/heart/VedioAdapter.kt
+++ app/src/main/java/com/example/instagram5/ui/heart/VedioAdapter.kt
@@ -3,8 +3,13 @@ package com.example.instagram5.ui.heart
 import android.content.Context
 import android.provider.MediaStore
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.instagram5.databinding.EachVedioBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.PlaybackException
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.source.MediaSource
 
 
 class VideoAdapter(
@@ -15,6 +20,23 @@ class VideoAdapter(
     class VideoViewHolder ( val binding: EachVedioBinding,
                             var context: Context,
                             var videoPreparedListener: OnVideoPreparedListener): RecyclerView.ViewHolder(binding.root) {
+
+        private lateinit var exoPlayer:ExoPlayer
+        private lateinit var mediaSource:MediaSource
+
+
+        fun setVedioPath(url:String){
+            exoPlayer=ExoPlayer.Builder(context).build()
+
+            exoPlayer.addListener(object : Player.Listener{
+
+                override fun onPlayerError(error: PlaybackException) {
+                    super.onPlayerError(error)
+                    Toast.makeText(context, "Can't play this video", Toast.LENGTH_SHORT).show()
+                }
+
+            })
+        }
 
     }
 
